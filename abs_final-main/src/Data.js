1adeed993dd651ab3fc913bacc61fe1c3505953e
balaji0611 <balajikrish666@gmail.com>
2024-09-03 23:19:57 +0530

    ABS

---
+++ abs_final-main/src/Data.js
@@ -0,0 +1,667 @@
+let shopItemsData = [
+   {
+      id:"item_1",
+      name:"10 CM ELECTRIC SPARKLERS",
+      price:12,
+      desc:"SPARKLERS",
+      img:"images/10cmElectric.jpeg"
+   },
+   {
+      id:"item_2",
+      name:"10 CM CRACKLING SPARKLERS",
+      price:25,
+      desc:"SPARKLERS",
+      img:"images/10cmCrackling.jpeg"
+   },
+   {
+      id:"item_3",
+      name:"10 CM GREEN SPARKLERS",
+      price:18,
+      desc:"SPARKLERS",
+      img:"images/10cmGreen.jpeg"
+   },
+  
+   {
+      id:"item_4",
+      name:"10 CM RED SPARKLERS",
+      price:33,
+      desc:"SPARKLERS",
+      img:"images/10cmRed.jpeg"
+   },
+   {
+      id:"item_5",
+      name:"15 CM ELECTRIC SPARKLERS",
+      price:35,
+      desc:"SPARKLERS",
+      img:"images/15cmElectric.jpeg"
+   },
+   {
+      id:"item_6",
+      name:"15 CM COLOR SPARKLERS",
+      price:45,
+      desc:"SPARKLERS",
+      img:"images/15cmColor.jpeg"
+   },
+   {
+      id:"item_7",
+      name:"15 CM GREEN SPARKLERS",
+      price:56,
+      desc:"SPARKLERS",
+      img:"images/15cmGreen.jpeg"
+   },
+   {
+      id:"item_8",
+      name:"15 CM RED SPARKLERS",
+      price:56,
+      desc:"SPARKLERS",
+      img:"images/15cmRed.jpeg"
+   },
+   {
+      id:"item_9",
+      name:"30 CM ELECTRIC SPARKLERS",
+      price:180,
+      desc:"SPARKLERS",
+      img:"images/30cmElectric.png"
+   },
+   {
+      id:"item_10",
+      name:"30 CM CRACKLING SPARKLERS",
+      price:200,
+      desc:"SPARKLERS",
+      img:"images/30cmCrackling.jpeg"
+   },
+   {
+      id:"item_11",
+      name:"30 CM RED SPARKLERS",
+      price:200,
+      desc:"SPARKLERS",
+      img:"images/30cmRed.jpeg"
+   },
+   {
+      id:"item_12",
+      name:"30 CM GREEN SPARKLERS",
+      price:200,
+      desc:"SPARKLERS",
+      img:"images/30cmGreen.jpeg"
+   },
+   {
+      id:"item_13",
+      name:"50 CM ELECTRIC SPARKLERS",
+      price:200,
+      desc:"SPARKLERS",
+      img:"images/50cmElectric.jpeg"
+   },
+   {
+      id:"item_14",
+      name:"50 CM COLOR SPARKLERS",
+      price:200,
+      desc:"SPARKLERS",
+      img:"images/50cmColor.jpeg"
+   },
+   {
+      id:"item_15",
+      name:"Annam \/ Mini Deluxe ( 5 PCS )",
+      price:60,
+      desc:"MATCH BOX",
+      img:"images/matches.jpeg"
+   },
+   {
+      id:"item_16",
+      name:"Majesty Mega",
+      price:140,
+      desc:"MATCH BOX",
+      img:"images/majestyMega.jpeg"
+   },
+   {
+      id:"item_17",
+      name:"CHAKKARAM BIG (10 PCS)",
+      price:40,
+      desc:" GROUND CHAKKARAS",
+      img:"images/gcBig.jpeg"
+   },
+   {
+      id:"item_18",
+      name:"CHAKKARAM BIG (25 PCS)",
+      price:80,
+      desc:" GROUND CHAKKARAS",
+      img:"images/gcSpl.jpeg"
+   },
+   {
+      id:"item_19",
+      name:"CHAKKARAM DELUXE",
+      price:100,
+      desc:" GROUND CHAKKARAS",
+      img:"images/gcDLX.jpeg"
+   },
+   
+   
+   {
+      id:"item_20",
+      name:"FLOWER POT BIG",
+      price:80,
+      desc:"FLOWER POT",
+      img:"images/fpBig.jpeg"
+   },
+   {
+      id:"item_21",
+      name:"FLOWER POT SPECIAL",
+      price:100,
+      desc:"FLOWER POT",
+      img:"images/fpSpl.jpeg"
+   },
+   
+   {
+      id:"item_22",
+      name:"FLOWER POT",
+      price:200,
+      desc:"FLOWER POT ASHOKA",
+      img:"images/fpAshoka.jpeg"
+   },
+   {
+      id:"item_23",
+      name:"COLOUR KOTI ",
+      price:300,
+      desc:"FLOWER POT",
+      img:"images/colorKoti.jpeg"
+   },
+   {
+      id:"item_24",
+      name:"COLOUR KOTI DLX",
+      price:600,
+      desc:"FLOWER POT",
+      img:"images/colorKotiDlx.jpeg"
+   },
+   {
+      id:"item_25",
+      name:"1 1\\2\" TWINKLING STAR",
+      price:40,
+      desc:"TWINKLING STAR",
+      img:"images/tsSmall.jpeg"
+   },
+   {
+      id:"item_26",
+      name:"4\" TWINKLING STAR DELUXE",
+      price:80,
+      desc:"TWINKLING STAR",
+      img:"images/tsLarge.jpeg"
+   },
+   {
+      id:"item_27",
+      name:"2 3\/4\" KURUVI",
+      price:12,
+      desc:" CRACKERS",
+      img:"images/kuruvi.jpeg"
+   },
+   {
+      id:"item_28",
+      name:"3 1\\2\" LAKSHMI",
+      price:18,
+      desc:" CRACKERS",
+      img:"images/lakshmi.jpeg"
+   },
+   {
+      id:"item_29",
+      name:"4 1\/2\" LAKSHMI",
+      price:25,
+      desc:" CRACKERS",
+      img:"images/lakshmi.jpeg"
+   },
+   {
+      id:"item_30",
+      name:"4\" GOLD LAKSHMI",
+      price:40,
+      desc:" CRACKERS",
+      img:"images/goldLakshmi.jpeg"
+   },
+   {
+      id:"item_31",
+      name:"5\" JALLIKATTU",
+      price:40,
+      desc:" CRACKERS",
+      img:"images/jallikattu.jpeg"
+   },
+   {
+      id:"item_32",
+      name:"BULLET BOMB",
+      price:40,
+      desc:"BOMB",
+      img:"images/bulletBomb.jpeg"
+   },
+   {
+      id:"item_33",
+      name:"HYDROGEN BOMB",
+      price:80,
+      desc:"BOMB",
+      img:"images/hydrogenBomb.jpeg"
+   },
+   {
+      id:"item_34",
+      name:"CLASSIC BOMB",
+      price:160,
+      desc:"BOMB",
+      img:"images/classicBomb.jpeg"
+   },
+  
+   {
+      id:"item_35",
+      name:"MEGA DIGITAL BOMB",
+      price:220,
+      desc:"BOMB",
+      img:"images/digitalBomb.jpeg"
+   },
+   {
+      id:"item_36",
+      name:"28 CHORSA",
+      price:15,
+      desc:" GARLAND AND CHORSA",
+      img:"images/wala.jpeg"
+   },
+   {
+      id:"item_37",
+      name:"56 GIANT",
+      price:55,
+      desc:" GARLAND AND CHORSA",
+      img:"images/wala.jpeg"
+   },
+   {
+      id:"item_38",
+      name:"50 DELUXE",
+      price:120,
+      desc:" GARLAND AND CHORSA",
+      img:"images/wala.jpeg"
+   },
+   {
+      id:"item_39",
+      name:"100 WALA",
+      price:60,
+      desc:" GARLAND AND CHORSA",
+      img:"images/wala.jpeg"
+   },
+   {
+      id:"item_40",
+      name:"1K",
+      price:350,
+      desc:" GARLAND AND CHORSA",
+      img:"images/wala.jpeg"
+   },
+   {
+      id:"item_41",
+      name:"2K",
+      price:800,
+      desc:" GARLAND AND CHORSA",
+      img:"images/wala.jpeg"
+   },
+   {
+      id:"item_42",
+      name:"5K",
+      price:1800,
+      desc:" GARLAND AND CHORSA",
+      img:"images/wala.jpeg"
+   },
+   {
+      id:"item_43",
+      name:"10K",
+      price:3500,
+      desc:" GARLAND AND CHORSA",
+      img:"images/wala.jpeg"
+   },
+   {
+      id:"item_44",
+      name:"CHOTA FANCY",
+      price:60,
+      desc:"MEGA FANCY",
+      img:"images/chotaFancy(1).jpeg"
+   },
+   {
+      id:"item_45",
+      name:"2\" COMET",
+      price:150,
+      desc:"MEGA FANCY",
+      img:"images/2inchPipe.jpeg"
+   },
+   {
+      id:"item_46",
+      name:"2\" COMET (3 PCS)",
+      price:400,
+      desc:"MEGA FANCY",
+      img:"images/2inch3Piece.jpeg"
+   },
+   {
+      id:"item_47",
+      name:"3 1\/2\" COMET",
+      price:300,
+      desc:"MEGA FANCY",
+      img:"images/2inchPipe.jpeg"
+   },
+   {
+      id:"item_48",
+      name:"4\" COMET",
+      price:500,
+      desc:"MEGA FANCY",
+      img:"images/nayagara.jpeg"
+   },
+   {
+      id:"item_49",
+      name:"12 SHOT MULTI FUNCTION",
+      price:220,
+      desc:"SKY SHOT",
+      img:"images/12Shot.jpeg"
+   },
+   {
+      id:"item_50",
+      name:"12 SHOT MULTI COLOR",
+      price:220,
+      desc:"SKY SHOT",
+      img:"images/12Shot.jpeg"
+   },
+   
+   {
+      id:"item_51",
+      name:"30 SHOT MULTI COLOR",
+      price:300,
+      desc:"SKY SHOT",
+      img:"images/30Shot.jpeg"
+   },
+   {
+      id:"item_52",
+      name:"60 SHOT MULTI COLOUR",
+      price:500,
+      desc:"SKY SHOT",
+      img:"images/60Shot.jpeg"
+   },
+   {
+      id:"item_53",
+      name:"120 SHOT MULTI COLOUR",
+      price:1000,
+      desc:"SKY SHOT",
+      img:"images/120Shot.jpeg"
+   },
+   {
+      id:"item_54",
+      name:"240 SHOT MULTI COLOUR",
+      price:2000,
+      desc:"SKY SHOT",
+      img:"images/240Shot.jpeg"
+   },
+   {
+      id:"item_55",
+      name:"SET OUT",
+      price:3900,
+      desc:"SKY SHOT",
+      img:"images/setOut.jpeg"
+   },
+   {
+      id:"item_56",
+      name:"KITKAT",
+      price:30,
+      desc:"FANCY FOUNTAINS",
+      img:"images/kitKat.jpeg"
+   },
+   {
+      id:"item_57",
+      name:"MAGIC STONE",
+      price:25,
+      desc:"FANCY FOUNTAINS",
+      img:"images/electricStone.jpeg"
+   },
+   {
+      id:"item_58",
+      name:"BUTTERFLY",
+      price:100,
+      desc:"FANCY FOUNTAINS",
+      img:"images/butterfly.jpeg"
+   },
+   {
+      id:"item_59",
+      name:"VIOLET MATRIX",
+      price:50,
+      desc:"FANCY FOUNTAINS",
+      img:"images/violetMatrix.jpeg"
+   },
+   {
+      id:"item_60",
+      name:"WELCOME ANGEL",
+      price:170,
+      desc:"FANCY FOUNTAINS",
+      img:"images/welcomeAngel.jpeg"
+   },
+   {
+      id:"item_61",
+      name:"HELICOPTER",
+      price:150,
+      desc:"FANCY FOUNTAINS",
+      img:"images/helicopter.jpeg"
+   },
+   {
+      id:"item_62",
+      name:"GANGA JAMUNA",
+      price:200,
+      desc:"FANCY FOUNTAINS",
+      img:"images/gangaJamuna.jpeg"
+   },
+   {
+      id:"item_63",
+      name:"PHOTO FLASH",
+      price:120,
+      desc:"FANCY FOUNTAINS",
+      img:"images/photoFlash.jpeg"
+   },
+   {
+      id:"item_64",
+      name:"MAYAJAAL",
+      price:150,
+      desc:"FANCY FOUNTAINS",
+      img:"images/mayajaal.jpg"
+   },
+   {
+      id:"item_65",
+      name:"SIREN",
+      price:150,
+      desc:"FANCY FOUNTAINS",
+      img:"images/siren.jpeg"
+   },
+   {
+      id:"item_66",
+      name:"MEGA SIREN",
+      price:250,
+      desc:"FANCY FOUNTAINS",
+      img:"images/siren.jpeg"
+   },
+   {
+      id:"item_67",
+      name:"BAMBARRAM",
+      price:150,
+      desc:"FANCY FOUNTAINS",
+      img:"images/bambaram.jpeg"
+   },
+   {
+      id:"item_68",
+      name:"TIM TIM",
+      price:120,
+      desc:"FANCY FOUNTAINS",
+      img:"images/timTim.jpeg"
+   },
+   {
+      id:"item_69",
+      name:"SKY WINGS",
+      price:250,
+      desc:"FANCY FOUNTAINS",
+      img:"images/skyWings.jpeg"
+   },
+   {
+      id:"item_70",
+      name:"SHINCHAN",
+      price:280,
+      desc:"FANCY FOUNTAINS",
+      img:"images/shinchan.jpeg"
+   },
+   {
+      id:"item_71",
+      name:"KULFI ( MEGA CRACKLING )",
+      price:400,
+      desc:"FANCY FOUNTAINS",
+      img:"images/kulfi.jpeg"
+   },
+   {
+      id:"item_72",
+      name:"VIOLET MATRIX",
+      price:500,
+      desc:"FANCY FOUNTAINS",
+      img:"images/violetMatrix.jpeg"
+   },
+   {
+      id:"item_73",
+      name:"POWER RANGERS",
+      price:250,
+      desc:"FANCY FOUNTAINS",
+      img:"images/powerRangers.jpg"
+   },
+   {
+      id:"item_74",
+      name:"BELLY DANCE PEACOCK",
+      price:200,
+      desc:"FANCY FOUNTAINS",
+      img:"images/peacock.jpeg"
+   },
+   {
+      id:"item_75",
+      name:"RAIN AND SHINE",
+      price:230,
+      desc:"FANCY FOUNTAINS",
+      img:"images/rainAndShine.jpg"
+   },
+   {
+      id:"item_76",
+      name:"POPEYE (TRICOLOUR FOUNTAIN)",
+      price:300,
+      desc:"FANCY FOUNTAINS",
+      img:"images/popeyee.jpeg"
+   },
+   {
+      id:"item_77",
+      name:"RED BLOOM",
+      price:200,
+      desc:"FANCY FOUNTAINS",
+      img:"images/redBloom.jpeg"
+   },
+   {
+      id:"item_78",
+      name:"TIN BEER",
+      price:150,
+      desc:"FANCY FOUNTAINS",
+      img:"images/tin.jpeg"
+   },
+   {
+      id:"item_79",
+      name:"TWINS STONE (2 STEP FUNCTION)",
+      price:175,
+      desc:"FANCY FOUNTAINS",
+      img:"images/twinsTone.jpg"
+   },
+   {
+      id:"item_80",
+      name:"RAINBOW COLOUR SMOKE",
+      price:300,
+      desc:"FANCY FOUNTAINS",
+      img:"images/rainbowSmoke.jpeg"
+   },
+   {
+      id:"item_81",
+      name:"SCOOBY DOO",
+      price:300,
+      desc:"FANCY FOUNTAINS",
+      img:"images/scoobyDoo.jpg"
+   },
+   {
+      id:"item_82",
+      name:"PAPER BOMB 1\/4 KG",
+      price:75,
+      desc:"FANCY FOUNTAINS",
+      img:"images/paperBomb.jpeg"
+   },
+   {
+      id:"item_83",
+      name:"PAPER BOMB 1\/2 KG",
+      price:150,
+      desc:"FANCY FOUNTAINS",
+      img:"images/paperBomb.jpeg"
+   },
+   
+   {
+      id:"item_84",
+      name:"GOLD BIJILI (50 PCS)",
+      price:20,
+      desc:"BIJILI",
+      img:"images/goldBijili.jpeg"
+   },
+   {
+      id:"item_85",
+      name:"STRIPPED BIJILI ",
+      price:40,
+      desc:"BIJILI",
+      img:"images/strippedBijili.jpeg"
+   },
+   {
+      id:"item_86",
+      name:"BABY ROCKET",
+      price:50,
+      desc:"ROCKET",
+      img:"images/babyRocket.jpeg"
+   },
+   {
+      id:"item_87",
+      name:"2 SOUND ROCKET",
+      price:150,
+      desc:"ROCKET",
+      img:"images/2soundRocket.jpeg"
+   },
+   {
+      id:"item_88",
+      name:"WHISTLING ROCKET",
+      price:200,
+      desc:"ROCKET",
+      img:"images/whistleRocket.jpeg"
+   },
+   {
+      id:"item_89",
+      name:"MERCURY BOX ",
+      price:325,
+      desc:" GIFT BOX",
+      img:"images/giftBox.jpeg"
+   },
+   {
+      id:"item_90",
+      name:"DISNEY BOX",
+      price:425,
+      desc:" GIFT BOX",
+      img:"images/giftBox.jpeg"
+   },
+   {
+      id:"item_91",
+      name:"FAMILY BOX",
+      price:625,
+      desc:" GIFT BOX",
+      img:"images/giftBox.jpeg"
+   },
+   {
+      id:"item_92",
+      name:"EXECUTIVE BOX",
+      price:1000,
+      desc:" GIFT BOX",
+      img:"images/giftBox.jpeg"
+   },
+   {
+      id:"item_93",
+      name:"EXECUTIVE DELUXE BOX",
+      price:1300,
+      desc:" GIFT BOX",
+      img:"images/giftBox.jpeg"
+   },
+   {
+      id:"item_94",
+      name:"VIP BOX",
+      price:1500,
+      desc:" GIFT BOX",
+      img:"images/giftBox.jpeg"
+   }
+];
